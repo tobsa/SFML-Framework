2f1102f559ef55931ddb801a7a267ccb9b1b1c10
Tobias Savinainen <tobias@savinainen.se>
2013-05-07 14:57:37 +0200

    Formating fixes

--- SFML Framework/Source/Scanner.cpp
+++ SFML Framework/Source/Scanner.cpp
@@ -10,100 +10,105 @@
 #include "Scanner.hpp"
 #include <cctype>
 
+namespace sfx
+{
+
 ////////////////////////////////////////////////////////////////////////////////
 Scanner::Scanner(const std::string& str) :
-	m_string (str)
+    m_string (str)
 {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 std::string Scanner::getToken()
 {
-	// If there's no tokens then return an empty string
-	if(!hasToken())
-		return "";
+    // If there's no tokens then return an empty string
+    if(!hasToken())
+        return "";
 
-	// The current character position
-	std::size_t current	= 0;
+    // The current character position
+    std::size_t current	= 0;
 
-	// Skip spaces
-	if(current < m_string.size())
-	{
-		while(isspace(m_string[current]))
-		{
-			current++;
-			if(current == m_string.size())
-				break;
-		}
-	}
+    // Skip spaces
+    if(current < m_string.size())
+    {
+        while(isspace(m_string[current]))
+        {
+            current++;
+            if(current == m_string.size())
+                break;
+        }
+    }
 
-	m_string = m_string.substr(current,m_string.size() - current);
-	current  = 0;
+    m_string = m_string.substr(current,m_string.size() - current);
+    current  = 0;
 
-	// If the current character is a number
-	if(isdigit(m_string[current]))
-	{
-		// Loop until the next non-number
-		while(isdigit(m_string[current]))
-		{
-			current++;
-			if(current == m_string.size())
-				break;
-		}
-	}
-	// If the current character is a letter
-	else if(isalpha(m_string[current]))
-	{
-		// Loop until the next non-letter
-		while(isalpha(m_string[current]))
-		{
-			current++;
-			if(current == m_string.size())
-				break;
-		}
-	}
-	else
-		current++;
-	
-	// Get the token
-	std::string token = m_string.substr(0,current);
+    // If the current character is a number
+    if(isdigit(m_string[current]))
+    {
+        // Loop until the next non-number
+        while(isdigit(m_string[current]))
+        {
+            current++;
+            if(current == m_string.size())
+                break;
+        }
+    }
+    // If the current character is a letter
+    else if(isalpha(m_string[current]))
+    {
+        // Loop until the next non-letter
+        while(isalpha(m_string[current]))
+        {
+            current++;
+            if(current == m_string.size())
+                break;
+        }
+    }
+    else
+        current++;
+    
+    // Get the token
+    std::string token = m_string.substr(0,current);
 
-	// Skip spaces
-	if(current < m_string.size())
-	{
-		while(isspace(m_string[current]))
-		{
-			current++;
-			if(current == m_string.size())
-				break;
-		}
-	}
+    // Skip spaces
+    if(current < m_string.size())
+    {
+        while(isspace(m_string[current]))
+        {
+            current++;
+            if(current == m_string.size())
+                break;
+        }
+    }
 
-	// Make sure the token is excluded from the string
-	m_string = m_string.substr(current,m_string.size() - current);
+    // Make sure the token is excluded from the string
+    m_string = m_string.substr(current,m_string.size() - current);
 
-	return token;
+    return token;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 std::string Scanner::peekToken()
 {
-	std::string token = getToken();
-	saveToken(token);
-	return token;
+    std::string token = getToken();
+    saveToken(token);
+    return token;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void Scanner::saveToken(const std::string& token)
 {
-	if(token == "") 
-		return;
+    if(token == "") 
+        return;
 
-	m_string = token + " " + m_string;
+    m_string = token + " " + m_string;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 bool Scanner::hasToken() const
 {
-	return !m_string.empty();
+    return !m_string.empty();
 }
+
+} // namespace sfx
