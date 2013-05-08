fbcf994e52a40b5930f2dc0f2c938c6a1734d80c
Tobias Savinainen <tobias@savinainen.se>
2013-05-08 13:43:45 +0200

    Added a general include file

---
+++ SFML Framework/Source/Sfx.hpp
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: Sfx.hpp
+// Author:   Tobias Savinainen
+// Year:     2013
+////////////////////////////////////////////////////////////////////////////////
+#ifndef SFX_HPP
+#define SFX_HPP
+
+////////////////////////////////////////////////////////////////////////////////
+// Header files
+////////////////////////////////////////////////////////////////////////////////
+#include "BaseProperty.hpp"
+#include "Config.hpp"
+#include "FileReader.hpp"
+#include "FileWriter.hpp"
+#include "GameSettings.hpp"
+#include "SFML Gui/GuiObject.hpp"
+#include "SFML Gui/Label.hpp"
+#include "Log.hpp"
+#include "SFML Gui/Picture.hpp"
+#include "Property.hpp"
+#include "ResourceManager.hpp"
+#include "Scanner.hpp"
+#include "Timer.hpp"
+#include "Utility.hpp"
+
+#endif
