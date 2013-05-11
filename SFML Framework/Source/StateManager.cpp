3162a53411962b2a7459e47411473a7aa0697efa
Tobias Savinainen <tobias@savinainen.se>
2013-05-11 20:14:32 +0200

    Added StateManager class

---
+++ SFML Framework/Source/StateManager.cpp
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: StateManager.cpp
+// Author:   Tobias Savinainen
+// Year:     2013
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Header files
+////////////////////////////////////////////////////////////////////////////////
+#include "StateManager.hpp"
+#include "Log.hpp"
+
+namespace sfx
+{
+
+////////////////////////////////////////////////////////////////////////////////
+StateManager::StateManager() :
+    m_currentState (nullptr)
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void StateManager::add(const std::string& key, StatePtr state)
+{
+    if(m_states.find(key) == m_states.end())
+        m_states[key] = state;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+StateManager::StatePtr StateManager::getState() const
+{
+    if(!m_currentState)
+    {
+        std::string error = "Error (StateManager::getState()): m_currentState == nullptr (You must set a starting state)";
+        sfx::Log::write(error);
+        throw error;
+    }
+
+    return m_currentState;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void StateManager::setState(const std::string& key)
+{
+    auto it = m_states.find(key);
+    if(it == m_states.end())
+    {
+        std::string error = "Error (StateManager::setState()): " + key + " doesn't exist";
+        sfx::Log::write(error);
+        throw error;
+    }
+
+    m_currentState = it->second;
+}
+
+}
