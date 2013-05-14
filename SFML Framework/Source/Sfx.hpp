7493d74600bf989c7467e932233cf7bd7805679e
Tobias Savinainen <tobias@savinainen.se>
2013-05-14 18:46:40 +0200

    Added MusicList.hpp to the include list

--- SFML Framework/Source/Sfx.hpp
+++ SFML Framework/Source/Sfx.hpp
@@ -25,6 +25,7 @@
 #include "Gui/GuiObject.hpp"
 #include "Gui/Label.hpp"
 #include "Log.hpp"
+#include "Audio/MusicList.hpp"
 #include "Gui/Picture.hpp"
 #include "Property.hpp"
 #include "ResourceManager.hpp"
